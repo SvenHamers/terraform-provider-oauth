9d1f32a262f8bf47aaa1873d4c05a4857a9880a1
SvenHamers <44347057+SvenHamers@users.noreply.github.com>
2021-01-04 09:26:52 +0100

    Update main.tf

--- examples/main.tf
+++ examples/main.tf
@@ -8,7 +8,7 @@ provider "oauth" {
 data "oauth_token" "example" {
       client_id = "xxxxxxx"
     client_secret = "xxxxx"
-    token_endpoint = "https://test.test.io/identity-server/connect/token"
+    token_endpoint = "https://test.test.io/oauth/connect/token"
     scopes = [""]
 }
 
